823997a246a798457bf4bd8bd1679a5110d21746
alicema15 <alicema@berkeley.edu>
2018-11-02 14:15:20 -0700

    loadData.py successfully inserts and retrieves documents from the mongoDB database

--- loadData.py
+++ loadData.py
@@ -1,3 +1,5 @@
+## This file contains inserting and deleting documents into the database as functions.
+
 from pymongo import MongoClient
 
 client = MongoClient();
@@ -5,19 +7,24 @@ db = client.EnronDB ## our DB is called EnronDB
 collection = db.emails ## create a collection within EnronDB called emails
 
 ### INSERT DOCUMENT ###
-
-f = open('maildir/allen-p/inbox/1.') ## opens a test file
+test_file_path = 'maildir/allen-p/inbox/1.'
+f = open(test_file_path) ## opens a test file
 text = f.read()
 
-print("text is here: ", text)
+def insert_doc(file_path, text):
+  file_document = {"file_path": file_path, "text": text}
+  print("text is here: ", text)
+  collection.insert(file_document)
 
-file_document = {"username": "allen-p", "text": text}
-collection.insert(file_document)
+insert_doc(test_file_path, text)
 
 ### RETRIEVE DOCUMENT ###
 
-retrieved_doc = collection.find_one({"username": "allen-p"}) 
+
+def retrieve_doc(file_path):
+  retrieved_doc = collection.find_one({"file_path": file_path})
+  print("retrieved doc: ", retrieved_doc)
+
+retrieve_doc(test_file_path)
 ## TO-DO: Retrieval of the document is working but need to make sure that 
 ## newlines aren't stored in such a weird way
-
-print("retrieved doc: ", retrieved_doc)
