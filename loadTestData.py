87cde4727028aec83638b6461eb48c282f3ebe5b
alicema15 <alicema@berkeley.edu>
2018-11-02 14:08:21 -0700

    created loadTestData.py which loads data successfully into EnronDB, ignored maildir enron emails

---
+++ loadTestData.py
@@ -0,0 +1,25 @@
+### This file is a reference created at 14:05 that loads a test document into mongoDB.
+
+from pymongo import MongoClient
+
+client = MongoClient();
+db = client.EnronDB ## our DB is called EnronDB
+collection = db.emails ## create a collection within EnronDB called emails
+
+### INSERT DOCUMENT ###
+
+f = open('maildir/allen-p/inbox/1.') ## opens a test file
+text = f.read()
+
+print("text is here: ", text)
+
+file_document = {"username": "allen-p", "text": text}
+collection.insert(file_document)
+
+### RETRIEVE DOCUMENT ###
+
+retrieved_doc = collection.find_one({"username": "allen-p"}) 
+## TO-DO: Retrieval of the document is working but need to make sure that 
+## newlines aren't stored in such a weird way
+
+print("retrieved doc: ", retrieved_doc)
